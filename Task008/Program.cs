2c4da80fdfd56685f2a25867a1d919d2ae5c2bc7
Anna <ankakras@yandex.ru>
2022-10-07 23:47:24 +0700

    Решила задачу 008

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -1,4 +1,15 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
 
-int namber = new Random().Next(10,99);
-Console.WriteLine(namber);
+int number = new Random().Next(10,99);
+string namber = Convert.ToString(Console.ReadLine());
+int size = 2;
+int i = 0;
+int max = number[0];
+while(i < size)
+{
+    if(number[i] > max)
+    {
+        max = number[i];
+    }
+    i = i + 1;
+}
