952820cc996bd4c1405efc91af9f1974b4d1aebc
Anna <ankakras@yandex.ru>
2022-10-07 23:12:42 +0700

    Решила задачу 006

---
+++ Task006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трехзначного числа
+
+Console.Write("Введите трехзначное число ");
+string namber = Convert.ToString(Console.ReadLine());
+int size = 3;
+int i = size;
+int x = namber[i];
+Console.WriteLine(x);
