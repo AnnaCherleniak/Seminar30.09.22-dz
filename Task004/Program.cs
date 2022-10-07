90c1beee61751d3dc8a88f67c0af4057f92ff789
Anna <ankakras@yandex.ru>
2022-10-07 17:19:08 +0700

    Решила задачу 004

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число четным
+Console.WriteLine ("Введите число");
+double n = Convert.ToDouble(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
