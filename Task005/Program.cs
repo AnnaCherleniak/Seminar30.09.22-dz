4b6e6a34278ccd2cfc98909f86ebbfb109cfe12f
Anna <ankakras@yandex.ru>
2022-10-07 17:29:02 +0700

    Решила задачу 005

---
+++ Task005/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать четные числа от 1 до N
+double i = 1;
+Console.Write("Введите число N");
+double N = Convert.ToDouble(Console.ReadLine());
+while (i <= N)
+{
+    if(i % 2 == 0)
+        Console.WriteLine(i);
+    i = i + 1;
+}
