2d465f5ec510fafbdfc2402d875ddf310fabfa04
Anna <ankakras@yandex.ru>
2022-10-07 19:58:06 +0700

    Решила задачу 010

---
+++ Task010/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выявить, кратно ли число заданному, если нет, вывести его остаток
+
+Console.WriteLine("Введите делимое");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите делитель");
+double n = Convert.ToDouble(Console.ReadLine());
+if (a % n == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else 
+{
+    Console.WriteLine(a % n);
+}
