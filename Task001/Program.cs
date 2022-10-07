64b5d47e6d5cfd508497b27050eb684636281b34
Anna <ankakras@yandex.ru>
2022-10-07 22:17:15 +0700

    Решила задачу 001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,1 +1,13 @@
 ﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine("Введите первое число");
+double f = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double s = Convert.ToDouble(Console.ReadLine());
+if(f == s * s)
+{
+    Console.WriteLine("является");
+}
+else
+{
+    Console.WriteLine("не является");
+}
