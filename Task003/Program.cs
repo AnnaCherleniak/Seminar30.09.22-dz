5fdf4fa38c3152c4ee25506cda34bca7c1d46465
Anna <ankakras@yandex.ru>
2022-10-07 15:58:25 +0700

    Решила задачу 003

---
+++ Task003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти максимальное из трех чисел
+Console.WriteLine ("Введите первое число");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ("Введите третье число");
+double c = Convert.ToDouble(Console.ReadLine());
+double max = a;
+if (b > max)
+{
+     max = b;
+}
+if (c > max)
+{
+     max = c;
+}
+Console.WriteLine("max " +max);
