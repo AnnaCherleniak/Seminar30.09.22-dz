0465f18093713e95132c7457d3b71f513450258d
Anna <ankakras@yandex.ru>
2022-10-08 23:45:31 +0700

    Черновик

--- Черновик/Program.cs
+++ Черновик/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите трехзначное число ");
+string number = Convert.ToString(Console.ReadLine());
+int max = number[0];
+if(number[1] > max)
+{
+    max = number[1];
+}
+if(number[2] > max)
+{
+    max = number[2];
+}
+Console.WriteLine(max);
