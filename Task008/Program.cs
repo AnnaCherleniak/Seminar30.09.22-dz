f8f03f1a6a65b9bb5c0734dc479081c213c8459a
Anna <ankakras@yandex.ru>
2022-10-08 22:38:39 +0700

    Решение задачи 008

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -1,2 +1,8 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
 
+Console.Write("Введите трехзначное число ");
+string number = Convert.ToString(Console.ReadLine());
+int size = 3;
+int i = 0;
+int max_number = number[i];
+Console.WriteLine(max_number);
