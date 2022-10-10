97a240320b8a5dcd21a8314962024c99b5093d67
Anna <ankakras@yandex.ru>
2022-10-10 16:09:17 +0700

    Решение задачи 011

---
+++ Task011/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите число ");
+double n = Convert.ToDouble(Console.ReadLine());
+if(n < 100)
+{
+    Console.WriteLine("НЕТ");
+
+}
+else 
+{
+    Console.WriteLine(n);
+}
+string number = Convert.ToString(Console.ReadLine());
+int size = number.Length;
+int i = size - 3;
+Console.WriteLine(number[i]);
