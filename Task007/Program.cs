8ba308bf2b66f836db94397223687bac3ea942c0
Anna <ankakras@yandex.ru>
2022-10-10 12:59:15 +0700

    Решение задачи 007

---
+++ Task007/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число ");
+string number = Convert.ToString(Console.ReadLine());
+int i = 1;
+Console.WriteLine(number[i]);
+
